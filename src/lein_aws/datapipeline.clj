c0672b96304768a17b8fcd018a5752e564bfa5fe
Steven Surgnier <ssurgnier@gmail.com>
2013-11-12 16:02:35 +0000

    Update lein-aws.datapipeline.

--- src/lein_aws/datapipeline.clj
+++ src/lein_aws/datapipeline.clj
@@ -6,14 +6,14 @@
 
 (defn get-definition
   "Get the pipeline definition for `pipeline-id`."
-  [project [pipeline-id]]
+  [project pipeline-id]
   (let [config (:aws project)]
     (with-credential [(:access-key config) (:secret-key config)]
       (pprint (aws/get-pipeline-definition {:pipeline-id pipeline-id})))))
 
 (defn activate-pipeline
   "Activate the pipeline with id `pipeline-id`."
-  [project [pipeline-id]]
+  [project pipeline-id]
   (let [config (:aws project)]
     (with-credential [(:access-key config) (:secret-key config)]
       (aws/activate-pipeline {:pipeline-id pipeline-id}))))
@@ -27,7 +27,7 @@
 
 (defn create-pipeline
   "Create a new data pipeline with `name` and `unique-id`."
-  [project [name]]
+  [project name]
   (let [config (:aws project)
         unique-id (.toString (java.util.UUID/randomUUID))]
     (with-credential [(:access-key config) (:secret-key config)]
@@ -36,7 +36,7 @@
 (defn put-pipeline
   "Upload a data pipeline definition defined in `path` with
    the key `pipeline-name`."
-  [project [pipeline-name path]]
+  [project pipeline-name path]
   (let [config (:aws project)
         pipeline-definitions (parse-flow path)
         pipeline (get pipeline-definitions (keyword pipeline-name))]
